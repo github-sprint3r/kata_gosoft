77d9f980455dd36bf28ef8d33e25a0ef547716d8
somkiat <somkiat.p@gmail.com>
2014-06-17 08:55:08 +0700

    Kata 2 :: Roman Numberic

---
+++ src/test/java/kata/gosoft/RomanNumericTest.java
@@ -0,0 +1,14 @@
+package kata.gosoft;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumericTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
